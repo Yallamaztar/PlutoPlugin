746aec5486df19e991030cbcec5281772dd35cf2
Yallamaztar <budi.world@yahoo.com>
2026-02-26 15:14:59 +0200

    Refactor: make the clean_log.py code cleaner

--- scripts/clean_log.py
+++ scripts/clean_log.py
@@ -1,13 +1,16 @@
 import re
 
-# cleans the log file from ANSI color codes using regex patterns
+def remove_ansi_colors(content: str) -> str:
+    return re.sub(r'\x1B\[[0-9;]*[mK]', '', content)
+
+def read_log(file: str) -> str:
+    with open(file, "r") as f: return f.read()
+
 def main() -> None:
     file = input("enter path to your log file: ")
-    with open(file, "r") as f:
-        content = f.read()
-
-    with open(file, "w") as f:
-        f.write(re.sub(r'\x1B\[[0-9;]*[mK]', '', content))
+    content = read_log(file)
+    with open(file, "w") as f: 
+        f.write(remove_ansi_colors(content))
 
 if __name__ == '__main__':
     main()
